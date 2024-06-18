337976780a926a8d008b587e13e0008262823c26
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2024-06-18 10:10:11 +0200

    removed content from root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-  import { base } from "$app/paths";
 </script>
 
-<div class="welcome">
-  <img src="{base}/explore2_logo_color.png" alt="" />
-  <div class="header">
-    Bienvenue dans <b>Explore 2</b> <i>en musique</i>
-  </div>
-</div>
-
 <style>
-  .welcome {
-    /* position: absolute;
-    inset: 0; */
-    background-color: white;
-    /* z-index: 10000000; */
-  }
 </style>
